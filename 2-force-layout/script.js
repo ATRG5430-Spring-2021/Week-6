f1846d681c58939c5b1fbb6de0f89873010d92b5
work <Work@Navarjuns-MBP.hitronhub.home>
2021-03-05 15:00:30 -0800

    file update

--- 2-force-layout/script.js
+++ 2-force-layout/script.js
@@ -11,66 +11,8 @@ d3.json('data/data.json')
 .then(function(data) {
     console.log(data);
 
-    let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-
-    let simulation = d3.forceSimulation(data.nodes)
-        .force('link', d3.forceLink(data.links).id(d => d.id))
-        .force('charge', d3.forceManyBody())
-        .force('center', d3.forceCenter(size.w / 2, size.h / 2));
-
-    let link = svg.append('g')
-        .attr('stroke', '#999')
-        .attr('stroke-opacity', 0.6)
-        .selectAll('line')
-        .data(data.links)
-        .join('line')
-        .attr('stroke-width', d => Math.sqrt(d.value));
-
-    let node = svg.append('g')
-        .attr('stroke', '#fff')
-        .attr('stroke-width', 1.5)
-        .selectAll('circle')
-        .data(data.nodes)
-        .join('circle')
-        .attr('r', 5)
-        .attr('fill', d => colorScale(d.group))
-        .call(drag(simulation));
-
-    simulation.on('tick', () => {
-        link
-            .attr('x1', d => d.source.x)
-            .attr('y1', d => d.source.y)
-            .attr('x2', d => d.target.x)
-            .attr('y2', d => d.target.y);
-    
-        node
-            .attr('cx', d => d.x)
-            .attr('cy', d => d.y);
-        });
 });
 
 
 function drag(simulation) {
-  
-    function dragstarted(event) {
-      if (!event.active) simulation.alphaTarget(0.3).restart();
-      event.subject.fx = event.subject.x;
-      event.subject.fy = event.subject.y;
-    }
-    
-    function dragged(event) {
-      event.subject.fx = event.x;
-      event.subject.fy = event.y;
-    }
-    
-    function dragended(event) {
-      if (!event.active) simulation.alphaTarget(0);
-      event.subject.fx = null;
-      event.subject.fy = null;
-    }
-    
-    return d3.drag()
-        .on('start', dragstarted)
-        .on('drag', dragged)
-        .on('end', dragended);
-  }
+}
